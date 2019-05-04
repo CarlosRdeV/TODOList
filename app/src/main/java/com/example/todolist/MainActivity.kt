5a3718cd13d944cfaaae692c73e5826cbf25bcad
carlosrdev <carlosrdev@gmail.com>
2019-05-04 03:23:45 -0500

    Creación de la clase pokemon
    Se creo la clase Pokemon y se pusieron los textview acorde al contenido, el checkbox.ischecked acorde al estado

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.CheckBox
 import android.widget.ListView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,21 +19,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var pk1 = Pokemon()
+        pk1.nombre="Charmander"
+        pk1.nivel=1
+        pk1.estado=false
+
+        var pk2 = Pokemon()
+        pk2.nombre="Bulbasaur"
+        pk2.nivel=11
+        pk2.estado=true
+
+        var pk3 = Pokemon()
+        pk3.nombre="Squirtle"
+        pk3.nivel=5
+        pk3.estado=false
+
+
+
         val listView = findViewById<ListView>(R.id.main_listview)
         //val color = Color.parseColor("#FF0000")
         //listView.setBackgroundColor(color)
 
-        listView.adapter = MyAdapter(this)
+
+        listView.adapter = MyAdapter(this,pk1,pk2,pk3)
 
     }
 
-    private class MyAdapter(context: Context) : BaseAdapter(){
+    private class MyAdapter(context: Context, pk1: Pokemon,pk2: Pokemon,pk3: Pokemon) : BaseAdapter(){
 
         private val mContext : Context
 
-        private val nombres = arrayListOf<String>(
-            "Pikachu","Charmander","Squirtle","Bulbasaur"
-        )
+
+        private val nombres = arrayListOf<Pokemon>(pk1,pk2,pk3)
 
         init {
             mContext = context
@@ -44,11 +62,15 @@ class MainActivity : AppCompatActivity() {
         val row_main = layoutInflater.inflate(R.layout.row_main,null,false)
 
         val positionTextView = row_main.findViewById<TextView>(R.id.position_textView)
-       positionTextView.text = "Row Number: $position"
+       //positionTextView.text = "Row Number: $position"
+            positionTextView.text="Nivel: ${nombres[position].nivel}"
 
             val nameTextView = row_main.findViewById<TextView>(R.id.name_textView)
-            nameTextView.text = nombres.get(position)
+            //nameTextView.text = nombres.get(position).toString()
+            nameTextView.text = nombres[position].nombre
 
+            val checkBoxEstatus = row_main.findViewById<CheckBox>(R.id.checkBoxEstatus)
+            checkBoxEstatus.isChecked = nombres[position].estado!!
 
             return row_main
             //val textView = TextView(mContext)
---
+++ app/src/main/java/com/example/todolist/Pokemon.kt
@@ -0,0 +1,7 @@
+package com.example.todolist
+
+class Pokemon {
+     var nombre:String?=null
+     var nivel:Int?=0
+     var estado:Boolean?=false
+}
